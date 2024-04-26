e30660bfe082cfd6737da4c5aab35b241f9938b4
Manuwest21 <hammers178@hotmail.com>
2024-04-26 11:30:14 +0200

    one

---
+++ main.py
@@ -0,0 +1,11 @@
+from fastapi import FastAPI
+from fastapi.routing import APIRouter
+from utils2 import app as app_base2
+from modelisation1 import has_access, generate_token
+
+router = APIRouter()
+router.include_router(app_base2)
+
+app = FastAPI()
+
+app.include_router(router)
