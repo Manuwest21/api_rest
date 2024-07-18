7fd1f5368a39e31a9ebf6973ea9c194388844fc3
Manuwest21 <hammers178@hotmail.com>
2024-07-18 19:29:26 +0200

    test ci

--- main.py
+++ main.py
@@ -1,11 +1,11 @@
-from fastapi import FastAPI
-from fastapi.routing import APIRouter
-from utils2 import app as app_base2
-from modelisation1 import has_access, generate_token
+# from fastapi import FastAPI
+# from fastapi.routing import APIRouter
+# # from utils22 import app as app_base2
+# from modelisation1 import has_access, generate_token
 
-router = APIRouter()
-router.include_router(app_base2)
+# router = APIRouter()
+# router.include_router(app_base2)
 
-app = FastAPI()
+# app = FastAPI()
 
-app.include_router(router)
+# app.include_router(router)
