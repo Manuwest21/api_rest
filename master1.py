67779ef930fa8b97680e2eae2298cb6f3e979e8c
Manuwest21 <hammers178@hotmail.com>
2024-07-19 10:21:22 +0200

    test branch3

--- master1.py
+++ master1.py
@@ -6,6 +6,6 @@ from utils22 import router1 as utils_router
 router = APIRouter()
 router.include_router(modelisation1_router)
 router.include_router(utils_router, prefix="/utils", tags=["utils"])
-
+#alphonse#
 app = FastAPI()
 app.include_router(router)
